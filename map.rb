3904d039ba3b472cb13c4a06fbbb43d44d478f52
vivit <kazyu.vanilla@gmail.com>
2016-12-20 23:26:18 +0900

    第二回向けバージョンアップ

--- map.rb
+++ map.rb
@@ -1,30 +1,67 @@
 require 'dxruby'
 
-CASTLE = Image.load('castle.png')
-FOREST = Image.load('forest.png')
-MOUNTAIN = Image.load('mountain.png')
-LAND = Image.load('land.png')
-HERO = Image.load('hero.png')
-map = []
-tile = [LAND, FOREST, MOUNTAIN, CASTLE]
+#四方を向くようにする
+class Sprite
+  attr_accessor :direction
 
-open('map.txt') do |file|
+  def move(x,y)
+    self.x += x
+    self.y += y
+    if x > 0 
+      @direction = 2
+    elsif x < 0 
+      @direction = 1
+    elsif y > 0 
+      @direction = 0
+    elsif y < 0 
+      @direction = 3
+    end
+    self.image = HERO[@direction]
+ end
+
+end
+
+CASTLE = Image.load('img/castle.png')
+FOREST = Image.load('img/forest.png')
+MOUNTAIN = Image.load('img/mountain.png')
+LAND = Image.load('img/land.png')
+HERO = Image.load_tiles('img/hero.png',1,4)
+CAVE = Image.load('img/cave.png')
+CAVE_MAP = Image.load_tiles('img/cave_map.png',3,4)
+
+map = {:field => [], :cave => []}
+current_map = :field
+tile = {:field => [LAND, FOREST, MOUNTAIN, CASTLE], :cave => CAVE_MAP}
+
+open('field.txt') do |file|
   while l = file.gets
-    map << l.strip.split('')
+    map[:field] << l.strip.split('')
   end
 end
 
-x = 0
-y = 0
+open('cave.txt') do |file|
+  while l = file.gets
+    map[:cave] << l.strip.split('')
+  end
+end
+
+hero = Sprite.new(0,0)
+hero.direction = 0
+cave_entrance = Sprite.new(300,200,CAVE)
 
 Window.loop do
   20.times do |i|
     15.times do |j|
-      Window.draw(i*32, j*32, tile[map[j][i].to_i])
+      Window.draw(i*32, j*32, tile[current_map][map[current_map][j][i].to_i])
     end
   end
-  x += Input.x
-  y += Input.y
-  Window.draw(x, y, HERO)
+  cave_entrance.draw
+  hero.move(Input.x,Input.y)
+  hero.draw
+  if hero === cave_entrance
+    current_map = :cave
+    hero.x = 320
+    hero.y = 480
+  end
 
 end
--- stg.rb
+++ stg.rb
@@ -5,45 +5,57 @@ size = 32
 r = 0
 ORIGIN_X = 320
 ORIGIN_Y = 40
-x = 320 - size/2
-y = 400
-BALL = Image.load('ball.png')
-PLAYER = Image.load('player.png')
-AIRPLANE = Image.load('airplane.png')
+BALL = Image.load('img/ball.png')
+PLAYER = Image.load('img/player.png')
+AIRPLANE = Image.load('img/airplane.png')
 theta = 0
 v = 0
 balls = []
 count = 0
 
-class Ball
+plane = Sprite.new(320-size/2,400,AIRPLANE)
+
+class Ball < Sprite
 attr_reader :d
   def initialize(theta)
-    @x = ORIGIN_X - 16
-    @y = ORIGIN_Y
+    self.x = ORIGIN_X - 16
+    self.y = ORIGIN_Y
     @d = 0
-    @theta = theta * PI / 180.0
+    @theta = theta
+    self.image = BALL
   end
 
   def move
     @d += 8
-    Window.draw(@d*cos(@theta)+@x, @d*sin(@theta)+@y, BALL)
+    self.x = @d*cos(@theta) + ORIGIN_X - 16
+    self.y = @d*sin(@theta) + ORIGIN_Y
+    self.draw
   end
 end
 
 Window.loop do
+  Window.draw(ORIGIN_X-35, ORIGIN_Y, PLAYER)
   count += 1
-  if balls.size < 15 && count%10 == 0
-    balls << Ball.new(90)
-    balls << Ball.new(60)
-    balls << Ball.new(120)
+  if count > 60*1 && balls.size < 15 && count%10 == 0
+    theta = atan2(plane.y - ORIGIN_Y,plane.x - ORIGIN_X + size/2) - 2 * PI
+    balls << Ball.new(theta)
+    balls << Ball.new(theta + 30 * PI / 180.0)
+    balls << Ball.new(theta - 30 * PI / 180.0)
+  elsif balls.size < 15 && count%10 == 0
+    balls << Ball.new(90 * PI / 180.0)
+    balls << Ball.new(60 * PI / 180.0)
+    balls << Ball.new(120 * PI / 180.0)
   end
+  
   balls.each do |ball|
     ball.move
     balls.delete(ball) if(ball.d > 480)
   end
-  x += Input.x*3
-  y += Input.y*3
-  Window.draw(x, y, AIRPLANE)
-  Window.draw(ORIGIN_X-35, ORIGIN_Y, PLAYER)
+  plane.x += Input.x*3
+  plane.y += Input.y*3
+  plane.draw
+  if plane === balls
+    #ここに衝突したときの処理を書く
+  end
 end
 
